bcbddbcc1cb0c161819500e647c742176a21ceb9
mrinmoymena <mrinmoymena@gmail.com>
2020-02-19 13:47:14 +0530

    Update install.sh
    Uncommented virtual service and destination rule

--- install.sh
+++ install.sh
@@ -29,8 +29,8 @@ sed -i "s/latest/$DOCKER_TAG/g" bookinfo/platform/kube/bookinfo_v2.yaml
 sudo /usr/bin/kubectl apply -f bookinfo/platform/kube/bookinfo_v2.yaml
 
 # add support for version 2 in virtual service and destination rule along with request weight
-#sudo /usr/bin/kubectl apply -f bookinfo/networking/virtual-service-demo-app-v2.yaml
-#sudo /usr/bin/kubectl apply -f bookinfo/networking/destination-rule-demo-app-v2.yaml
+sudo /usr/bin/kubectl apply -f bookinfo/networking/virtual-service-demo-app-v2.yaml
+sudo /usr/bin/kubectl apply -f bookinfo/networking/destination-rule-demo-app-v2.yaml
 
 else
 
